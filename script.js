65db7e7feda8327c660b97deb98432fd4b353ce9
banlonjones <banlonjones@go-groups.net>
2022-01-05 12:03:29 +0100

    linter error fix

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 let books = [];
 
-let bookList = document.querySelector(".book-list");
-let addBookForm = document.querySelector("#add-book");
-let add = document.querySelector("#add");
-const {title, author} = addBookForm.elements;
+const bookList = document.querySelector('.book-list');
+const addBookForm = document.querySelector('#add-book');
+const add = document.querySelector('#add');
+const { title, author } = addBookForm.elements;
 
 // displaying books in localStorage
-if(localStorage.getItem('books')) {
+if (localStorage.getItem('books')) {
   console.log(JSON.parse(localStorage.getItem('books')));
   books = JSON.parse(localStorage.getItem('books'));
-}else {
+} else {
   console.log("failed");
 }
-display();
 
 function display() {
   books.forEach((item) => {
@@ -22,19 +21,19 @@ function display() {
     <button class="btn-remove" value="${item.id}"> remove </button>
     <hr>
     </div>`;
-    });
+  });
 }
 
+display();
+
 function addBook(name, author) {
-  id =  Math.floor(Math.random()*1000);
+  let id = Math.floor(Math.random() * 1000);
   books.push({name, author, id});
-  localStorage.setItem("books", JSON.stringify(books));
-  /*bookList.innerHTML = ``;
-  display();*/
+  localStorage.setItem('books', JSON.stringify(books));
   location.reload();
 }
 
-add.addEventListener("click", (e)=>{
+add.addEventListener('click', (e)=>{
   e.preventDefault();
   addBook(title.value, author.value);
   console.log(books);
@@ -44,12 +43,12 @@ function removeBook(id) {
   books = books.filter((item) => {
     return item.id != id;
   });
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
   location.reload();
 }
 
 
-let remove = document.querySelectorAll(".btn-remove");
+const remove = document.querySelectorAll('.btn-remove');
 remove.forEach((item) => {
   item.addEventListener('click', (e) => {
     e.preventDefault();
